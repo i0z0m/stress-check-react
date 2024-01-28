92769f9ffee9b00f22b135d1ddf1402cb6c96633
i0z0m <i0z0mu@gmail.com>
2024-01-29 01:15:47 +0900

    Improved back button design

--- src/BackButtons.tsx
+++ src/BackButtons.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 interface BackButtonsProps {
   onBack: () => void;
   onBackToTitle: () => void;
@@ -8,8 +6,8 @@ interface BackButtonsProps {
 const BackButtons: React.FC<BackButtonsProps> = ({ onBack, onBackToTitle }) => {
   return (
     <div>
-      <button type="button" onClick={onBackToTitle}>タイトル画面に戻る</button>
-      <button type="button" onClick={onBack}>一つ前の画面に戻る</button>
+      <button type="button" className="m-2 hover:opacity-50" onClick={onBackToTitle}>◀タイトル画面に戻る</button>
+      <button type="button" className="m-2 hover:opacity-50" onClick={onBack}>◀一つ前の画面に戻る</button>
     </div>
   );
 }
