9c03a34b0cdb2c443814bad861171413a33dd677
i0z0m <i0z0mu@gmail.com>
2024-01-29 01:28:43 +0900

    Center the next button

--- src/StartSection.tsx
+++ src/StartSection.tsx
@@ -10,7 +10,7 @@ const StartSection: React.FC<StartSectionProps> = ({ description, onNext }) => {
   return (
     <div>
       <h2>{description}</h2>
-      <button type="button" onClick={onNext} className="px-4 py-2 bg-blue-300 text-white rounded shadow-lg hover:bg-blue-400 transition-colors duration-300">次へ</button>
+      <button type="button" onClick={onNext} className="px-4 py-2 bg-blue-300 text-white rounded shadow-lg hover:bg-blue-400 transition-colors duration-300 m-2 mx-auto block">次へ</button>
     </div>
   );
 };
