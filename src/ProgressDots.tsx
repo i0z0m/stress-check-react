90b3eb2dd17e51337e76afaeb63fce6425078501
i0z0m <i0z0mu@gmail.com>
2024-01-29 15:21:02 +0900

    Change dotStyle to responsive design

--- src/ProgressDots.tsx
+++ src/ProgressDots.tsx
@@ -1,6 +1,6 @@
 // ProgressDots.tsx
 import React from 'react';
-import { flexCenterClass } from './styles';
+import { flexCenterClass, dotStyle } from './styles';
 
 interface ProgressDotsProps {
   questionIndex: number;
@@ -11,7 +11,7 @@ const ProgressDots: React.FC<ProgressDotsProps> = ({ questionIndex, totalQuestio
   return (
     <div className={flexCenterClass}>
       {Array.from({ length: totalQuestions }, (_, i) => `dot-${i + totalQuestions}`).map((key, index) => (
-        <div className="dotStyle" style={{backgroundColor: index <= questionIndex ? '#008CBA' : '#bbb'}} />
+        <div className={dotStyle} style={{backgroundColor: index <= questionIndex ? '#008CBA' : '#bbb'}} />
       ))}
     </div>
   );
--- src/styles.ts
+++ src/styles.ts
@@ -1,5 +1,6 @@
 // styles.ts
 export const buttonClass = "px-4 py-2 rounded shadow-lg transition-colors duration-300 border-2 m-2 bg-blue-100 text-teal-500 border-blue-300 hover:bg-blue-50";
+export const dotStyle = "h-2.5 w-2.5 rounded-full m-1 sm:h-3 sm:w-3 sm:m-1.5";
 export const backButtonClass = "m-2 hover:opacity-50";
 export const flexCenterClass = "flex justify-center";
 export const flexColumnRowClass = "flex flex-col sm:flex-row";
