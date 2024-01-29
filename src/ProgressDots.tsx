b016bf60b8c1676d64b1d327bb30acf46ffc34ea
i0z0m <i0z0mu@gmail.com>
2024-01-29 15:29:52 +0900

    Supports responsive design when there are many dots

--- src/ProgressDots.tsx
+++ src/ProgressDots.tsx
@@ -1,6 +1,6 @@
 // ProgressDots.tsx
 import React from 'react';
-import { flexCenterClass, dotStyle } from './styles';
+import { dotsContainerStyle, dotStyle } from './styles';
 
 interface ProgressDotsProps {
   questionIndex: number;
@@ -9,7 +9,7 @@ interface ProgressDotsProps {
 
 const ProgressDots: React.FC<ProgressDotsProps> = ({ questionIndex, totalQuestions }) => {
   return (
-    <div className={flexCenterClass}>
+    <div className={dotsContainerStyle}>
       {Array.from({ length: totalQuestions }, (_, i) => `dot-${i + totalQuestions}`).map((key, index) => (
         <div className={dotStyle} style={{backgroundColor: index <= questionIndex ? '#008CBA' : '#bbb'}} />
       ))}
