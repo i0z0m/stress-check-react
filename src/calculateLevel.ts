6a672c985f294172dbec4df255f10040d5568a5a
i0z0m <i0z0mu@gmail.com>
2024-02-05 21:35:02 +0900

    Remove redundant variables

--- src/calculateLevel.ts
+++ src/calculateLevel.ts
@@ -3,9 +3,7 @@ export function calculateLevel(scores: number[], values: number[][]): { method1:
   const method1 = scores[2] >= 77 || (scores[1] + scores[3] || 0) === 76 && scores[2] >= 63;
 
   const totals = values.map(value => value?.reduce((a, b) => a + b, 0) || 0);
-  const section2Total = totals[2];
-  const section1And3Total = totals[1] + totals[3];
-  const method2 = section2Total <= 12 || (section1And3Total <= 26 && section2Total <= 17);
+  const method2 = totals[2] <= 12 || (totals[1] + totals[3] <= 26 && totals[2] <= 17);
 
   return { method1, method2, totals };
 }
