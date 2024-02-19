e0cfb02687aac83da504163f9887fd9f2b4db87e
i0z0m <i0z0mu@gmail.com>
2024-02-19 12:30:52 +0900

    Modified the design to display and fill in lines on radar charts

--- src/ShowRadar.tsx
+++ src/ShowRadar.tsx
@@ -2,10 +2,10 @@
 import React from 'react';
 import { Radar } from 'react-chartjs-2';
 
-import { Chart, RadialLinearScale, PointElement, LineElement, ArcElement } from 'chart.js';
+import { Chart, RadialLinearScale, PointElement, LineElement, ArcElement, Filler } from 'chart.js';
 
 // 必要な要素とスケールを登録
-Chart.register(RadialLinearScale, PointElement, LineElement, ArcElement);
+Chart.register(RadialLinearScale, PointElement, LineElement, ArcElement, Filler );
 
 interface Factor {
   scale: string;
@@ -23,17 +23,15 @@ const ShowRadar: React.FC<ShowRadarProps> = ({ factors, level }) => {
     datasets: [
       {
         data: factors.map((factor) => factor.value),
-        borderColor: level === 'high' ? ['rgba(255, 0, 0, 1)'] : ['rgba(0, 0, 255, 1)'],
-        backgroundColor: level === 'high' ? ['rgba(255, 0, 0, 0.2)'] : ['rgba(0, 0, 255, 0.2)'],
-        pointBackgroundColor: level === 'high' ? ['rgba(255, 0, 0, 1)'] : ['rgba(0, 0, 255, 1)'],
-        pointBorderColor: level === 'high' ? ['rgba(255, 0, 0, 1)'] : ['rgba(0, 0, 255, 1)'],
-        pointHoverBackgroundColor: level === 'high' ? ['rgba(255, 0, 0, 1)'] : ['rgba(0, 0, 255, 1)'],
-        pointHoverBorderColor: level === 'high' ? ['rgba(255, 0, 0, 1)'] : ['rgba(0, 0, 255, 1)'],
-        pointRadius: 3,
-        pointHitRadius: 10,
-        lineTension: 0.3,
-        fill: true,
-        showLine: true,
+        borderWidth: 1,
+        borderColor: level === 'high' ? ['rgba(255, 99, 132, 1)'] : ['rgba(100, 149, 237, 1)'],
+        backgroundColor: level === 'high' ? ['rgba(255, 99, 132, 0.2)'] : ['rgba(100, 149, 237, 0.2)'],
+        pointBackgroundColor: level === 'high' ? ['rgba(255, 99, 132, 1)'] : ['rgba(100, 149, 237, 1)'],
+        pointBorderColor: level === 'high' ? ['rgba(255, 99, 132, 1)'] : ['rgba(100, 149, 237, 1)'],
+        pointHoverBackgroundColor: level === 'high' ? ['rgba(255, 99, 132, 1)'] : ['rgba(100, 149, 237, 1)'],
+        pointHoverBorderColor: level === 'high' ? ['rgba(255, 99, 132, 1)'] : ['rgba(100, 149, 237, 1)'],
+        pointRadius: 2,
+        pointHitRadius: 8,
       },
     ],
   };
