6eede3484547b6b2c5a33d5fe3bad7d65b980b8d
i0z0m <i0z0mu@gmail.com>
2024-02-22 15:45:01 +0900

    Fixed Warning: "ReactDOM.render is no longer supported in React 18. Use createRoot instead."

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Global } from '@emotion/react';
 import { globalStyles } from './styles';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Global styles={globalStyles} />
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const root = document.getElementById('root');
+
+if (root !== null) {
+  createRoot(root).render(
+    <React.StrictMode>
+      <Global styles={globalStyles} />
+      <App />
+    </React.StrictMode>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
