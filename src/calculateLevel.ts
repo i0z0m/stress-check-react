022dadf98a7c5a9f4b148377b8ae654ebee70744
i0z0m <i0z0mu@gmail.com>
2024-02-22 15:14:25 +0900

    Modify the calculation formula for evaluation using Method 1 using total pointso

--- src/calculateLevel.ts
+++ src/calculateLevel.ts
@@ -1,6 +1,6 @@
 // calculateLevel.ts
 export function calculateLevel(scores: number[], values: number[][]): { method1: boolean, method2: boolean, totals: number[] } {
-  const method1 = scores[2] >= 77 || (scores[1] + scores[3] || 0) === 76 && scores[2] >= 63;
+  const method1 = scores[2] >= 77 || (scores[1] + scores[3]) >= 76 && scores[2] >= 63;
 
   const totals = values.map(value => value?.reduce((a, b) => a + b, 0) || 0);
   const method2 = totals[2] <= 12 || (totals[1] + totals[3] <= 26 && totals[2] <= 17);
